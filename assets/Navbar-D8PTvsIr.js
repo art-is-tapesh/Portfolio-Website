0b8e3646672c803b60ee8ae98455e3428eaf0cfb
art-is-tapesh <tapesh1998@gmail.com>
2026-04-04 05:05:37 +0530

    Updates

--- assets/Navbar-D8PTvsIr.js
+++ assets/Navbar-D8PTvsIr.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-BMrFIuOx.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j,r as bt}from"./index-DRJ2Bqk5.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
